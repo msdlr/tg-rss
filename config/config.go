8d7071d31c4abbe720fdb8d30163de72525101f0
msdlr <miguelsrosa97@gmail.com>
2026-07-31 00:05:29 +0200

    Update configuration loading

--- config/config.go
+++ config/config.go
@@ -3,22 +3,30 @@ package config
 import (
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
 )
 
+var Configs Config
+
 type Config struct {
 	TelegramToken       string
-	UpdatePeriodMinutes uint
+	UpdatePeriodMinutes uint8
 }
 
-func LoadConfig() *Config {
+func LoadConfig() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found, using environment variables")
 	}
 
-	return &Config{
-		TelegramToken: os.Getenv("TELEGRAM_BOT_TOKEN"),
+	Configs.TelegramToken = os.Getenv("TELEGRAM_BOT_TOKEN")
+	period, err := strconv.Atoi(os.Getenv("UPDATE_PERIOD_MINUTES"))
+	if err != nil {
+		// Handle error - environment variable not set or invalid
+		period = 5 // default value
+	} else {
+		Configs.UpdatePeriodMinutes = uint8(period)
 	}
 }
